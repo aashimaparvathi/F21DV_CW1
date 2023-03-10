b0e1360d32c774e20d9b823fd52cd886bed4b951
Aashima <aashimaparvathi@gmail.com>
2023-03-10 18:50:41 +0000

    Added bubble chart

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 // Define the dimensions and margins of the plot
 var margin = { top: 80, right: 30, bottom: 30, left: 5 },
+  bubbleMargin = { top: 20, bottom: 50, left: 50, right: 30 },
   svgWidth = 800 - margin.left - margin.right,
-  svgHeight = 500 - margin.top - margin.bottom;
+  svgHeight = 500 - margin.top - margin.bottom,
+  bubbleWidth = 450 - bubbleMargin.left - bubbleMargin.right,
+  bubbleHeight = 500 - bubbleMargin.top - bubbleMargin.bottom,
+  bubbleChart,
+  bubbleX,
+  bubbleXValues,
+  bubbleXTicks,
+  bubbleY,
+  bubbleYValues,
+  bubbleYTicks,
+  bubbleTickFormatter,
+  bubbleR,
+  bubbleColor,
+  variableBubbleColor,
+  readyForBubble = 0,
+  bubbleID,
+  bubbleIsoCode,
+  countryColor,
+  firstTime = 0,
+  wave1year = 2020,
+  wave1month = 3,
+  wave2year = 2020,
+  wave2month = 9,
+  wave3year = 2021,
+  wave3month = 5;
+const intervalDelay = 1000; // change intervalDelay back to 1000 later
 
 const tooltip = d3
   .select("body")
@@ -9,6 +35,32 @@ const tooltip = d3
   .attr("class", "tooltip-c")
   .style("opacity", 0)
   .style("position", "absolute");
+
+const bubbletooltip = d3
+  .select("body")
+  .append("div")
+  .attr("class", "bubbletooltip-c")
+  .style("opacity", 0)
+  .style("position", "absolute");
+
+// create a div to hold the location or flag image
+let locationImage = d3
+  .select("body")
+  .append("div")
+  .attr("class", "location-image")
+  .style("position", "absolute")
+  .style("pointer-events", "none")
+  .style("opacity", 0);
+
+var myCountries = [
+  { location: "USA", iso_code: "USA" },
+  { location: "China", iso_code: "CHN" },
+  { location: "Australia", iso_code: "AUS" },
+  { location: "Guyana", iso_code: "GUY" },
+  { location: "Central African Republic", iso_code: "CAF" },
+];
+
+var myIsoCodes = ["USA", "CHN", "AUS", "GUY", "CAF"];
 
 var svg = d3
   .select("body")
@@ -19,6 +71,12 @@ var svg = d3
   //.style('background-color', '#eeeeee')
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+const bubbleSvg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", bubbleWidth + bubbleMargin.left + bubbleMargin.right)
+  .attr("height", bubbleHeight + bubbleMargin.top + bubbleMargin.bottom);
 
 d3.select(".play-c").style("margin-left", svgWidth / 2 + "px");
 
@@ -35,7 +93,7 @@ const path = d3.geoPath().projection(projection);
 const formatNumber = d3.format(".2s");
 const formatTime = d3.timeFormat("%B, %Y");
 
-var dataForAMonth_g = {};
+var dataForAMonth_g = {}; // global variable
 
 function filterData(month, year, data) {
   //console.log(data);
@@ -63,16 +121,23 @@ function filterData(month, year, data) {
         ? 0
         : parseFloat(d.total_deaths),
       new_cases: isNaN(parseFloat(d.new_cases)) ? 0 : parseFloat(d.new_cases),
-      total_cases: isNaN(parseFloat(d.total_cases))
+      total_cases: isNaN(parseFloat(+d.total_cases))
         ? 0
         : parseFloat(d.total_cases),
       month: d.month,
       year: d.year,
+      gdp_per_capita: +d.gdp_per_capita,
+      stringency_index: isNaN(parseFloat(d.stringency_index))
+        ? 0
+        : parseFloat(d.stringency_index),
+      population_density: +d.population_density,
     };
   });
 
   return dataForAMonth_map;
 }
+
+// ---------------------------------------------------------------------------------------------------
 
 Promise.all([
   // https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson
@@ -107,31 +172,13 @@ Promise.all([
 
   var yearToFilter = 2020;
   var monthToFilter = 1;
+
   let intervalId = null;
   let index = 0;
   var count = 0;
   const maxCount = 39,
-    intervalDelay = 100, // change intervalDelay back to 1000 later
     casesByCountry = {};
   var periodItem = svg;
-
-  /*
-  Define a color scale
-  const colorScale = d3
-    .scaleThreshold()
-    .range(["#fee5d9", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26"])
-    //.range(["#e5b2b1", "#f26c6c", "#c94d4d", "#b43d3d", "#a93434"])
-    .domain([
-      d3.min(monthly, (d) => d.new_cases),
-      d3.max(monthly, function (d) {
-        return d.new_cases;
-      }) * 0.0025,
-      d3.max(monthly, (d) => d.new_cases) * 0.075,
-      d3.max(monthly, (d) => d.new_cases) * 0.15,
-      d3.max(monthly, (d) => d.new_cases) * 0.75,
-      d3.max(monthly, (d) => d.new_cases),
-    ]);
-  */
 
   const colorScale = d3
     .scaleThreshold()
@@ -221,6 +268,15 @@ Promise.all([
     .attr("stroke", "#666666")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+  var locationimage = countries
+    .append("image")
+    .attr("class", "location-image")
+    .attr("id", (d) => d.iso_code + "img")
+    .attr("xlink:href", "./images/icon.png")
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("opacity", 0);
+
   // Set up the play button
   const button = d3.select("#play-button");
 
@@ -261,18 +317,33 @@ Promise.all([
       tooltip.transition().duration(0).style("opacity", 0);
     });
 
+  // ---------------------------------------------------------------------------------------------------
   button.on("click", function () {
     index = 0;
     if (intervalId) {
+      console.log("PAUSED");
       // If the interval is already running, stop it
       clearInterval(intervalId);
       intervalId = null;
       button.text("▶");
+      if (firstTime >= 1) {
+        bubbleChart.remove();
+      }
+      if (monthToFilter == 1) {
+        monthToFilter = 12;
+        yearToFilter--;
+      } else {
+        monthToFilter--;
+      }
+
+      renderBubble(yearToFilter, monthToFilter);
     } else if (count < maxCount) {
+      console.log("STARTED");
       intervalId = setInterval(intervalFunction, intervalDelay);
       console.log(intervalId);
       button.text("⏸");
     } else {
+      console.log("STOPPED");
       // Otherwise, start the interval
       intervalId = setInterval(intervalFunction, intervalDelay);
       console.log(intervalId);
@@ -282,15 +353,15 @@ Promise.all([
 
   function intervalFunction() {
     count++;
-    console.log("Function called " + count + " times");
+    //console.log("Function called " + count + " times");
 
-    console.log(
-      "Data for year: " + yearToFilter + "and month: " + monthToFilter
-    );
+    // console.log(
+    //   "Data for year: " + yearToFilter + "and month: " + monthToFilter
+    // );
     dataForAMonth = monthly.filter(
       (d) => d.year === yearToFilter && d.month === monthToFilter
     );
-    console.log(dataForAMonth);
+    // console.log(dataForAMonth);
 
     periodItem.text(formatTime(new Date(yearToFilter, monthToFilter - 1)));
     updateColors(yearToFilter, monthToFilter, dataForAMonth);
@@ -310,21 +381,23 @@ Promise.all([
     if (count >= maxCount) {
       console.log("Clearing interval and resetting");
       clearInterval(intervalId);
-      yearToFilter = 2020;
-      monthToFilter = 1;
       button.text("▶");
       intervalId = 0;
       count = 0;
+      readyForBubble = 1;
+      bubbleChart.remove();
+      renderBubble(2023, 3);
+      yearToFilter = 2020;
+      monthToFilter = 1;
     }
   }
 
   // Create a function to update the colors
   function updateColors(year, month, filteredData) {
     // console.log("Data for year: " + year + "and month: " + month);
-    // filteredData = monthly.filter((d) => d.year === year && d.month === month);
 
-    console.log("filteredData in updateColors");
-    console.log(filteredData);
+    //console.log("filteredData in updateColors");
+    //console.log(filteredData);
     // Create a mapping of new_cases by iso_code
 
     filteredData.forEach((d) => (casesByCountry[d.iso_code] = d.new_cases));
@@ -344,6 +417,208 @@ Promise.all([
       .attr("stroke", "#ffffff");
   }
 
+  // ---------------------------------------------------------------------------------------------------
+
+  function renderBubble(year, month) {
+    console.log("month and year in renderBubble: " + month + "," + year);
+
+    dataForAMonth_b = filterData(month, year, monthly);
+
+    var myDataForAMonth_b = dataForAMonth_b.filter(function (d) {
+      //console.log(d.iso_code);
+      return myIsoCodes.includes(d.iso_code);
+    });
+
+    console.log(myDataForAMonth_b);
+
+    bubbleX = d3
+      .scaleLinear()
+      .domain([
+        0,
+        //d3.min(myDataForAMonth_b, (d) => d.stringency_index),
+        d3.max(myDataForAMonth_b, (d) => d.stringency_index) * 1.5,
+      ])
+      .range([0, bubbleWidth]);
+
+    bubbleY = d3
+      .scaleLog()
+      .domain([
+        d3.min(myDataForAMonth_b, (d) => {
+          return d.total_cases > 0 ? d.total_cases : 0.1;
+        }),
+        d3.max(myDataForAMonth_b, (d) => d.total_cases) * 10,
+      ])
+      .range([bubbleHeight, 0]);
+
+    // Create a tick formatter function that formats the ticks uniformly
+    bubbleTickFormatter = bubbleY.tickFormat(10, "~s");
+
+    bubbleR = d3
+      .scaleLinear()
+      .domain([0, d3.max(myDataForAMonth_b, (d) => d.population_density)])
+      .range([20, 60]);
+
+    /* For reference: ["USA", "CHN", "AUS", "GUY", "CAF"]; */
+    bubbleColor = d3
+      .scaleOrdinal()
+      .domain(myIsoCodes)
+      .range(d3.schemeCategory10);
+    //      .range(["#1f77b4", "#ff7f0e", "#2ca02c", "#42ecff", "#9467bd"]);
+
+    function countryColor(iso_code) {
+      return iso_code === "USA" ||
+        iso_code === "CHN" ||
+        iso_code === "AUS" ||
+        iso_code === "GUY" ||
+        iso_code === "CAF"
+        ? bubbleColor(iso_code)
+        : "#7f7f7f";
+    }
+
+    bubbleChart = bubbleSvg
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + bubbleMargin.left + "," + bubbleMargin.top + ")"
+      );
+
+    bubbleSvg
+      .append("text")
+      .attr("x", bubbleWidth / 2 + bubbleMargin.left + 20)
+      .attr("y", bubbleMargin.top + 10 / 2)
+      .attr("text-anchor", "middle")
+      .attr("class", "bubble-title-c")
+      .attr("id", "covid-response")
+      .text("Impact of Stringency Measures");
+
+    bubbleChart
+      .selectAll("circle")
+      .data(myDataForAMonth_b)
+      .join("circle")
+      .attr("id", (d) => d.iso_code + "-bubble")
+      .attr("class", "countrybubbles")
+      .attr("cx", (d) => bubbleX(d.stringency_index))
+      .attr("cy", (d) =>
+        d.total_cases == 0 ? bubbleY(0.1) : bubbleY(d.total_cases)
+      )
+      .transition()
+      .duration(500)
+      .delay((d, i) => i * 100)
+      .attr("r", (d) => bubbleR(d.population_density))
+      .attr("stroke", (d) => bubbleColor(d.iso_code))
+      .attr("fill", (d) => bubbleColor(d.iso_code))
+      .attr("fill-opacity", "0.1")
+      .attr("stroke-opacity", "0.7");
+
+    const xAxis = d3.axisBottom(bubbleX);
+    const yAxis = d3.axisLeft(bubbleY).tickFormat(bubbleTickFormatter);
+
+    bubbleChart
+      .append("g")
+      .attr("transform", `translate(0, ${bubbleHeight})`)
+      .call(xAxis);
+
+    bubbleChart.append("g").call(yAxis);
+
+    bubbleChart
+      .append("text")
+      .attr("x", bubbleWidth / 2)
+      .attr("y", bubbleHeight + bubbleMargin.top + bubbleMargin.bottom / 2 - 2)
+      .attr("text-anchor", "middle")
+      .attr("class", "bubble-xaxis-c")
+      .attr("id", "stringency-index")
+      .text("Stringency Index");
+
+    bubbleChart
+      .append("text")
+      .attr("x", -bubbleHeight / 2)
+      .attr("y", -bubbleMargin.left + 10)
+      .attr("transform", "rotate(-90)")
+      .attr("text-anchor", "middle")
+      .attr("class", "bubble-yaxis-c")
+      .attr("id", "total-cases")
+      .text("Total Cases");
+
+    d3.selectAll(".countrybubbles")
+      .on("mouseover", function (event, d) {
+        (bubbleID = d3.select(this).attr("id")), (bubbleIsoCode = d.iso_code);
+        console.log(bubbleID + ", " + bubbleIsoCode);
+        d3.select(this).style("opacity", 0.7);
+        bubbletooltip
+          .html(
+            "<div class=bubbletooltip-text>" +
+              d.location +
+              "<br> Cases: " +
+              (typeof d.total_cases == "undefined"
+                ? 0
+                : d.total_cases.toFixed(0)) +
+              "<br> Population Density: " +
+              d.population_density +
+              "</div>"
+          )
+          .style("left", event.pageX + 2 + "px")
+          .style("top", event.pageY + 2 + "px")
+          .style("position", "absolute")
+          .style("padding", "0 5px") // This means 0px at the top and bottom
+          // and 10 on the left and right
+          .style("background-color", "white")
+          .style("border-color", function (d) {
+            var color = d3.color(bubbleColor(bubbleIsoCode));
+            color.opacity = 0.5;
+            return color.toString();
+          })
+          .style("border-style", "solid")
+          .style("border-width", "1px")
+          .style("border-radius", "10px")
+          .transition()
+          .duration(100)
+          .style("opacity", 1);
+
+        countryColor = d3.select("#" + bubbleIsoCode).attr("fill");
+
+        d3.select("#" + bubbleIsoCode)
+          .classed("country-highlight", true)
+          .attr("stroke", bubbleColor(bubbleIsoCode))
+          .attr("fill", bubbleColor(bubbleIsoCode));
+
+        //d3.select("#" + bubbleIsoCode + "img").style("opacity", 1);
+
+        // // get the bounding box of the country on the map
+        // let node = d3.select("#" + bubbleIsoCode).node();
+        // let bbox = node.getBBox();
+        // let transform = node.getAttribute("transform");
+        // if (transform) {
+        //   let transformValues = transform.match(/[-.\d]+/g).map(Number);
+        //   bbox.x += transformValues[0];
+        //   bbox.x -= margin.left;
+        //   bbox.y += transformValues[1];
+        //   bbox.y -= margin.top;
+        // }
+
+        // //set the location image src and alt attributes based on the location or flag data
+        // locationImage
+        //   .html(`<img src="./images/icon.png" alt="${d.location}"/>`)
+        //   .style("left", bbox.x + bbox.width - margin.left / 2 + "px")
+        //   .style("top", bbox.y + bbox.height + margin.top / 2 + "px")
+        //   .style("opacity", 1);
+      })
+      .on("mouseout", function (event, d) {
+        d3.select(this).style("opacity", 1);
+        bubbletooltip.transition().duration(0).style("opacity", 0);
+
+        d3.select("#" + bubbleIsoCode)
+          .classed("country-highlight", false)
+          .attr("stroke", "none")
+          .attr("fill", countryColor);
+
+        // locationImage.style("opacity", 0);
+      });
+
+    firstTime++;
+  }
+
+  // ---------------------------------------------------------------------------------------------------
+
   const brush = d3
     .brush()
     .extent([
@@ -361,6 +636,12 @@ Promise.all([
 
   // callback function for the brush
   function brushed(event) {
+    console.log(myIsoCodes);
+    myIsoCodes_orig = myIsoCodes;
+    myIsoCodes = myIsoCodes.filter((d) => false);
+    // console.log("brushed array: ");
+    // console.log(myIsoCodes);
+
     if (event.selection) {
       const selection = event.selection;
       var [[x0, y0], [x1, y1]] = selection;
@@ -379,8 +660,12 @@ Promise.all([
         })
         .map(function (d) {
           console.log("selected: " + d.id);
+          myIsoCodes.push(d.id);
           return d.id;
         });
+
+      console.log("new brushed array: ");
+      console.log(myIsoCodes);
 
       // update the display to show the details of only those countries
       countries.attr("fill", function (d) {
@@ -389,6 +674,15 @@ Promise.all([
           ? colorScale(casesByCountry[d.id])
           : "gray";
       });
+
+      if (bubbleChart) {
+        bubbleChart.remove();
+      }
+      renderBubble(yearToFilter, monthToFilter);
+    } else {
+      myIsoCodes = myIsoCodes_orig.filter((d) => true);
     }
   }
 });
+
+// ---------------------------------------------------------------------------------------------------
